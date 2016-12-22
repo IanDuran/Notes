536cb0916a39f90de87014200e0d4b142e9ec9f8
Ian Duran <ianduran60@gmail.com>
2016-12-22 13:06:14 -0600

    Initial commit

---
+++ app/src/main/java/com/example/usuario/notes/NoteActivity.java
@@ -0,0 +1,10 @@
+package com.example.usuario.notes;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Usuario on 22/12/2016.
+ */
+
+public class NoteActivity extends AppCompatActivity {
+}
